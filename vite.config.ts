c7498220dd3ff07b4e0dc7b878105aa5fa5b6879
sa <sa@local>
2026-04-09 01:43:54 +0800

    fix: 设置 vite base 为 /shuangpin/ 修复 GitHub Pages 404

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/shuangpin/",
   plugins: [Vue()],
   test: {
     globals: true,
